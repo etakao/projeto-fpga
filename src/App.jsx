f2d904e9dee1238821b53836434ec8b2c8814ac5
Erick <ericktakao28@gmail.com>
2022-12-20 21:21:44 -0300

    feat: add final version
    Co-authored-by: Willian Sotocorno <Willendes@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
-import { X, Info } from "phosphor-react";
-import * as Dialog from '@radix-ui/react-dialog';
+import { Info } from "phosphor-react";
 
 import { trivia } from "./trivia.js";
 import './styles/app.scss';
@@ -15,45 +14,53 @@ function App() {
     "reveal"
   ];
 
-  let index = Math.floor(Math.random() * trivia.length);
-
+  const [incompletedTrivia, setIncompletedTrivia] = useState([]);
   const [triviaToGuess, setTriviaToGuess] = useState("");
-  const [triviaIndex, setTriviaIndex] = useState(index);
+  const [triviaIndex, setTriviaIndex] = useState("");
   const [guess, setGuess] = useState("");
   const [actualHint, setActualHint] = useState(0);
   const [isGuessed, setIsGuessed] = useState(false);
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [isInfosVisible, setIsInfosVisible] = useState(false);
-  const [guessed, setGuessed] = useState([]);
+  const [isGameCompleted, setIsGameCompleted] = useState(false);
 
-  useEffect(() => {
-    setTriviaToGuess(trivia[index]);
-  }, []);
+  let index = Math.floor(Math.random() * trivia.length);
 
-  function reset() {
-    console.log(guessed);
-
-    while (guessed.includes(index)) {
-      index = Math.floor(Math.random() * trivia.length);
-    }
-
+  function setup() {
     setTriviaIndex(index);
     setTriviaToGuess(trivia[index]);
+    setIncompletedTrivia(Array.from(Array(trivia.length).keys()));
+  }
+
+  function reset() {
+    let index = Math.floor(Math.random() * incompletedTrivia.length);
+
+    setTriviaIndex(incompletedTrivia[index]);
+    setTriviaToGuess(trivia[incompletedTrivia[index]]);
     setGuess("");
     setActualHint(0);
     setIsGuessed(false);
     setIsModalVisible(false);
+  }
 
+  function handleNextTrivia() {
+    if (incompletedTrivia.length === 0) {
+      setIsGameCompleted(true);
+      setIsModalVisible(false);
+      return;
+    }
+
+    reset();
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-
+    
     if (guess.toUpperCase() === triviaToGuess.answer.toUpperCase()) {
       setActualHint(5);
       setIsGuessed(true);
+      setIncompletedTrivia(incompletedTrivia.filter(number => number !== triviaIndex));
       setIsModalVisible(true);
-      setGuessed([...guessed, triviaIndex])
       return;
     }
 
@@ -66,25 +73,25 @@ function App() {
     setGuess("");
   }
 
-  function handleKeyPress(event) {
-    if(event.which === 27) {
-      setIsModalVisible(false);
-    }
-  }
-
   function handleImageClassName() {
     return imageClassNames[actualHint];
   }
 
+  useEffect(() => {
+    setup();
+  }, []);
+
   return (
     <div className="container">
-      <h1>Projeto de FPGA</h1>
+      <h1>Guess To Success</h1>
 
       <div className="options">
         <button type="button" onClick={() => setIsInfosVisible(true)} className="default-button">
           <Info />
         </button>
-        <button className={actualHint > 4 ? "show default-button" : "hide"} type="button" onClick={reset}>Próxima trivia</button>
+        <button className={actualHint > 4 ? "default-button" : "invisible-button"} type="button" onClick={handleNextTrivia}>
+          Próxima trivia
+        </button>
       </div>
 
       <div className="image-hint">
@@ -118,12 +125,14 @@ function App() {
             onChange={event => setGuess(event.target.value)} 
             required
             autoFocus 
+            autoComplete='off'
             disabled={(actualHint > 4) && true} 
           />
+          
           {isGuessed ? (
-            <button 
+            <button
               type="button"
-              onClick={() => setIsModalVisible(true)} 
+              onClick={() => setIsModalVisible(true)}
               className="default-button"
             >
               Ver resposta
@@ -140,12 +149,8 @@ function App() {
         </form>
       </div>
 
-      <div className={isModalVisible ? "overlay" : "hide"} onKeyPress={handleKeyPress}>
+      <div className={isModalVisible ? "overlay" : "invisible"}>
         <div className="modal">
-          <button type="button" onClick={() => setIsModalVisible(false)} className="close-button">
-            <X size={24} weight="bold" />
-          </button>
-
           <h2>{isGuessed ? "Acertou :)" : "Errou :("}</h2>
 
           <img 
@@ -157,35 +162,35 @@ function App() {
 
           <div className="action-buttons">
             <button type="button" onClick={() => setIsModalVisible(false)} className="default-button">Fechar</button>
-            <button type="button" onClick={reset} className="default-button">Próxima trivia</button>
+            <button type="button" onClick={handleNextTrivia} className="default-button">Próxima trivia</button>
           </div>
         </div>
-
-        {/* <Dialog.Root >
-          <Dialog.Trigger />
-          <Dialog.Portal>
-            <Dialog.Overlay />
-            <Dialog.Content>
-              <Dialog.Title>
-                {isGuessed ? "Acertou :)" : "Errou :("}
-              </Dialog.Title>
-
-              <Dialog.Description />
-              <Dialog.Close>
-
-              </Dialog.Close>
-            </Dialog.Content>
-          </Dialog.Portal>
-        </Dialog.Root> */}
       </div>
 
-      <div className={isInfosVisible ? "overlay" : "hide"}>
+      
+      <div className={isInfosVisible ? "overlay" : "invisible"}>
         <div className="modal">
-          <h1>O jogo</h1>
+          <h2>Sobre o jogo</h2>
 
-          <p>O jogo consistem em...</p>
+          <img src="/assets/INFO.gif" alt="About" />
+
+          <p>O jogo consiste em adivinhar o componente hardware baseado em 5 dicas,
+            que são desvendadas uma a uma a cada erro.</p>
+          <p>Há também uma imagem para ajudar, que fica mais nítida a cada erro.</p>
+          <p>Ao acertar o componente, você pode escolher prosseguir para uma 
+            próxima fase.</p>
 
           <button onClick={() => setIsInfosVisible(false)} className="default-button">Fechar</button>
+        </div>
+      </div>
+
+      <div className={isGameCompleted ? "overlay" : "invisible"}>
+        <div className="modal">
+          <img src="/assets/BRAVISSIMO.gif" alt="Applause" />
+          
+          <h2>PARABÉNS!</h2>
+
+          <span>Você completou todos os desafios!</span>
         </div>
       </div>
     </div>
--- src/trivia.js
+++ src/trivia.js
@@ -7,40 +7,40 @@ export const trivia = [
       "É apagada por luz ultra-violeta",
       "Em português,entende-se por: memória programável apagável somente de leitura"
     ],
-    image: "http://blog.maxieduca.com.br/wp-content/uploads/2017/10/EPROM.jpg",
+    image: "/assets/EPROM.jpg",
     answer: "EPROM"
   },
   {
     hints: [
       "Jogou no Cruzeiro",
-      "Jogou no Ajax",
       "Jogou no Barcelona",
+      "Foi Bola de Ouro em 1997",
       "Foi Campeão do Mundo em 2002",
-      "Jogou no Corinthians"
+      "________, joga muito no Corinthians"
     ],
-    image: "https://www.lance.com.br/files/article_main/uploads/2018/11/15/5bedcf90501e4.jpeg",
-    answer: "Ronaldo"
+    image: "/assets/RONALDO.webp",
+    answer: "RONALDO"
   },
   {
     hints: [
-      "",
-      "",
-      "",
-      "",
-      ""
+      "Emite os sinais de controle para os demais componentes do computador agirem e/ou realizarem alguma tarefa",
+      "Seu principal componente é diretamente relacionado a uma fala de Godon Moore, conhecida como Lei de Moore",
+      "Suas atividades podem ser divididas em dois grandes grupos: função processamento e função controleSuas atividades podem ser divididas em dois grandes grupos: função processamento e função controle",
+      "É um componente vital para o sistema de computação",
+      "É o responsavel por qualquer operação realizada por um computador"
     ],
-    image: "",
-    answer: ""
+    image: "/assets/PROCESSADOR.jpg",
+    answer: "PROCESSADOR"
   },
   {
     hints: [
       "É um tipo de memória não volátil",
       "É uma memória de acesso aleatório",
-      "Seus dados são armazenados permanentemente ou semi-permanentemente.",
+      "Seus dados são armazenados permanentemente ou semi-permanentemente",
       "Armazena dados que são usados repetidamente em instruções programadas como a inicialização e operação de um sistema.",
       "Seus dados podem ser lidos, mas não existe uma operação de escrita"
     ],
-    image: "http://1.bp.blogspot.com/-Bk60Dxu6gkQ/TiHQ4BI2JZI/AAAAAAAAAD4/yY8HxGSPKeE/w1200-h630-p-k-no-nu/rom.jpg",
+    image: "/assets/ROM.jpg",
     answer: "ROM"
   },
   {
@@ -51,7 +51,117 @@ export const trivia = [
       "Quando um dado é escrito em determinado endereço, o dado armazenado anteriormente nesse endereço é substituído pela novo",
       "Armazena dados de curta duração"
     ],
-    image: "https://www.audiovisualesdecolombia.com/wp-content/uploads/2019/03/MODULO-DE-MEMORIA-RAM-KINGSTON-TECHNOLOGY-DDR4-4GB-2400MHZ-CL17-288PIN-UDIMM.jpg",
+    image: "/assets/RAM.webp",
     answer: "RAM"
+  },
+  {
+    hints: [
+      "É um tipo de memória volátil",
+      "É uma memória pequena, mas de alta velocidade",
+      "Armazena instruções ou dados usados recentemente da memória principal",
+      "Seu primeiro nível geralmente é integrado ao chip do processador, tendo capacidade de armazenamento muito limitada (primária)",
+      "Seu segundo nível é um chip separado ou um conjunto de chips externos ao processador, com capacidade maior que o primeiro (secundária)"
+    ],
+    image: "/assets/CACHE.jpg",
+    answer: "CACHE"
+  },
+  {
+    hints: [
+      "É um tipo de memória",
+      "É uma memória não-volátil",
+      "Faz uso de fusíveis para armazenar bits",
+      "Uma vez programada, não pode ser alterada",
+      "São programadas pelo usuário de acordo com sua necessidade"
+    ],
+    image: "/assets/PROM.jpg",
+    answer: "PROM"
+  },
+  {
+    hints: [
+      "É uma memória não-volátil",
+      "É uma memória de leitura e escrita de alta capacidade de armazenamento de bits",
+      "Podem ser usadas no lugar de drivers de disquetes ou discos rígidos de pequenas capacidades",
+      "Possui as operações de programação, leitura e apagamento",
+      "Possui versões compactas para laptops e câmeras digitais"
+    ],
+    image: "/assets/FLASH.jpg",
+    answer: "FLASH"
+  },
+  {
+    hints: [
+      "É uma memória não-volátil",
+      "É usado para armazenamento em massa interno",
+      "Normalmente é usada como memória secundária",
+      "Sua primeira versão para microcomputadores, foi lançada em 1976 pela Seagate",
+      "Atualmente existem opções mais rápidas quanto a leitura e gravação de dados"
+    ],
+    image: "/assets/HD.jpg",
+    answer: "HD"
+  },
+  {
+    hints: [
+      "É usado somente para o armazenamento e o deslocamento de dados recebidos de uma fonte externa",
+      "Diferentemente de um contador, não possui uma sequência de estados específica, exceto em certas aplicações muito especializadas",
+      "Pode ser empregado para manter temporariamente a próxima instrução a ser executada.",
+      "Pode conter uma palavra a ser armazenada na memória ou enviada à unidade de E/S, ou é usada para receber uma palavra da memória ou de uma unidade de E/S.",
+      "Se localizam no interior de um microprocessador, dentro da CPU",
+    ],
+    image: "/assets/REGISTRADOR.jpg",
+    answer: "REGISTRADOR"
+  },
+  {
+    hints:[
+      "É uma sigla",
+      "É responsável pelo controle das atividades de todos os componentes do computador, através da emissão de pulsos elétricos (sinais de controle)",
+      "Pode ser separado em dois tipo, chamados de Hardwired e Microprogramada",
+      "Se comporta como um decodificador e é implementada usando portas lógicas.",
+      "Também utiliza uma microprograma e uma micro memoria dentro do processador ( contém micro-instruções dentro dele)",
+    ],
+    image: "/assets/UC.png",
+    answer: "UC"
+  },
+  {
+    hints: [
+      "É o componente responsável por interligar os demais componentes",
+      "No ____ de controle fluem sinais específicos da programação do sistema",
+      "O ____ de endereços é utilizado pelo processor para indicar onde buscar um dado, ou onde gravá-lo",
+      "Possuem diferentes tamanhos (em bits), variando de acordo com o modelo do processador, servindo de passagem para bits de informação (instruções ou dados)",
+      "Fisicamente, é composto por múltiplas linhas que servem como 'vias'"
+    ],
+    image: "/assets/BARRAMENTO.jpg",
+    answer: "BARRAMENTO"
+  },
+  {
+    hints: [
+      "Possui duas entradas de dados, ambas conectadas a uma saída pelo barramento interno de dados",
+      "Recebe sinais de controle para determinar qual operação será efetuada",
+      "Em processadores mais antigos, o barramento interligava esta a um registrador especial chamado de acumulador",
+      "É um aglomerado de circuitos lógicos e componentes eletrônicos simples que, quando combinados efetuam determinadas operações",
+      "É o dispositivo do computador que executa as operações matemáticas com os dados"
+    ],
+    image: "/assets/ULA.jpg",
+    answer: "ULA"
+  },
+  {
+    hints: [
+      "Dispositivo que encaminha informações de diversas fontes para um único destino",
+      "É composto de portas lógicas (principalmente AND)",
+      "Possui várias linhas de entrada e apenas uma de saída",
+      "A seleção da entrada é feita através de sinais de controle denominados variáveis de seleção",
+      "É conhecido como seletor de dados",
+    ],
+    image: "/assets/MULTIPLEXADOR.webp",
+    answer: "MULTIPLEXADOR"
+  },
+  {
+    hints: [
+      "É entendido como um dispositivo de controle",
+      "Uma analogia: Funciona como o chefe de uma embarcação a remo, que a coordena para que a mesma siga na direção correta",
+      "Sua base é um dispositivo gerador de pulsos (cristal de quartzo), cuja duração é chamada de ciclo",
+      "É um dispositivo cuja função é sincronizar as milhões de mudanças de estado por segundo que ocorrem nos circuitos de um processador (e certos outros componentes)",
+      "Sua frequência (mesmo que não exclusivamente) é um dos fatores que impactam no desempenho de um processador"
+    ],
+    image: "/assets/CLOCK.jpg",
+    answer: "CLOCK"
   },
 ]
